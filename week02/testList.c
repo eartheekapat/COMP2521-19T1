2d4104643b10cd2a04a728f612adfccebf6d5eb9
Kanadech J <kanadech.king@gmail.com>
2019-03-02 00:39:06 +1100

    Finished test1

--- week02/testList.c
+++ week02/testList.c
@@ -7,8 +7,14 @@
 #include <stdlib.h>
 #include "string.h"
 #include "DLList.h"
-
+/** Testing insert before and after 
+ * assert length and expected current value**/
 void test1 (DLList myList);
+
+/** Testing delete **/
+void test2 (DLList myList);
+
+/** Prints entire List in a certain format**/
 void printList (DLList myList);
 
 int main (void)
@@ -17,7 +23,8 @@ int main (void)
 	test1(myList);
 	return EXIT_SUCCESS;
 }
-
+/** Testing insert before and after 
+ * assert length and expected current value**/
 void test1 (DLList myList){
 	
 	size_t initialLength = DLListLength(myList);
@@ -25,69 +32,64 @@ void test1 (DLList myList){
 	char *a = "AAA";	// 1sr string
 	char *b = "BBB";	// 2nd string
 	char *c = "CCC";	// 3rd string
-	char temp[100];		// temp string for comparison
-	memset(temp, '\0', sizeof(temp));
+
+	printf("Showing the list before operations\n");
 	printf("Current-> %s\n",DLListCurrent(myList));
+	printf("nItem is %d\n",(int)DLListLength(myList));
+	printList(myList);
+	
 	printf("Moving Current halfway (%d)\n",(int)(initialLength/2));
 	DLListMove(myList, (int)(initialLength/2)); // Moves curr mid-list
 	printf("Current-> %s\n",DLListCurrent(myList));
-	printf("Insert a string %s after\n",a);
+	printf("Insert a string %s after curr\n",a);
 	DLListAfter(myList, a); // Inserts first string
-    assert(DLListLength(myList) == initialLength + 1);
+    assert(DLListLength(myList) == initialLength + 1); // Check for correct length
 	assert(validDLList(myList));
 	printList(myList);
-	printf("Current-> %s\n",DLListCurrent(myList));
 
-	// printf("Try moving current forward and backward 1 node\n");
-	// if (DLListMove(myList,1)) {
-	// 	printf("Current-> %s\n",DLListCurrent(myList));
-	// 	DLListMove(myList,-1);
-	// 	printf("Current-> %s\n",DLListCurrent(myList));
-	// 	assert(validDLList(myList));
-	// } else {
-	// 	printf("Can't move forward (End of list)\n");
-	// 	printf("Current-> %s\n",DLListCurrent(myList));
-	// 	assert(validDLList(myList));
-	// }
-	
+	printf("Current after 1st insertion-> %s\n",DLListCurrent(myList));	
 	printf("Moving current forward 1 node\n");
-	DLListMove(myList,1);
-	strcpy(temp,DLListCurrent(myList));
-	printf("Temp is %s and Current->%s\n",temp,DLListCurrent(myList));
-	DLListAfter(myList, "test1");
+	DLListMove(myList,1); // Move 1 node
+	printf("Doing 3 insertions\n");
+	DLListAfter(myList, a);
 	printf("Current-> %s\n",DLListCurrent(myList));
-	DLListAfter(myList, "test2");
+	assert(DLListLength(myList)==initialLength+2);
+	DLListAfter(myList, b);
 	printf("Current-> %s\n",DLListCurrent(myList));
-	DLListAfter(myList, "test3");
+	assert(DLListLength(myList)==initialLength+3);
+	DLListAfter(myList, c);
 	printf("Current-> %s\n",DLListCurrent(myList));
+	assert(DLListLength(myList)==initialLength+4);
 	printList(myList);
-	assert(strcmp(DLListCurrent(myList),"test3")==0);
-	printf("Delete last 3 nodes\n");
-	printf("B1Current-> %s\n",DLListCurrent(myList));
-	DLListDelete(myList);
-	printf("B2Current-> %s\n",DLListCurrent(myList));
-	DLListDelete(myList);
-	printf("B3Current-> %s\n",DLListCurrent(myList));
-	DLListDelete(myList);
-	printf("A3Current-> %s\n",DLListCurrent(myList));
-	printList(myList);
-	assert(strcmp(DLListCurrent(myList),temp)==0);
-	printList(myList);
+	assert(strcmp(DLListCurrent(myList),c)==0);
+
 	printf("Move back to first and insert %s at the beginning\n",b);
 	DLListMoveTo(myList,1);
 	DLListBefore(myList,b);
-	assert(DLListLength(myList)==initialLength+2);
+	assert(DLListLength(myList)==initialLength+5);
 	assert(strcmp(DLListCurrent(myList),b)==0);
 	printf("Current-> %s\n",DLListCurrent(myList));
 	assert(validDLList(myList));
 	printList(myList);
-	
+	printf("Moving Current halfway (%d)\n",(int)(DLListLength(myList)/2));
+	DLListMove(myList,(int)(DLListLength(myList)/2));
+	printf("Adding 'a line of text' before and after current\n");
+	DLListBefore(myList, "a line of text");
+	DLListMove(myList,1);
+	DLListAfter(myList, "a line of text");
+	DLListMove(myList,-2);
+	assert(strcmp(DLListCurrent(myList),"a line of text")==0);
+	printList(myList);
+	printf("nItem is %d\n",(int)DLListLength(myList));
+	assert(validDLList(myList));
 	printf("\nTest 1 Completed\n");
+
 	freeDLList (myList);
 }
 
+/** Prints entire List in a certain format**/
 void printList (DLList myList) {
 	printf("\n------\n");
 	putDLList (stdout, myList);
-	printf("------\n");
+	printf("------\n\n");
 }
