37b2feb058cc7df18c42e8ff11cd1ed5434e20ef
Kanadech J <kanadech.king@gmail.com>
2019-04-05 22:03:52 +1100

    DONE

--- week07/crawl.c
+++ week07/crawl.c
@@ -14,6 +14,7 @@
 #include "set.h"
 #include "stack.h"
 #include "url_file.h"
+#include "queue.h"
 
 #define BUFSIZE 1024
 
@@ -21,82 +22,83 @@ static void setFirstURL (char *, char *);
 
 int main (int argc, char **argv)
 {
-	URL_FILE *handle;
-	char buffer[BUFSIZE];
-	char baseURL[BUFSIZE];
-	char firstURL[BUFSIZE];
-	char next[BUFSIZE];
-	int maxURLs;
+    URL_FILE *handle;
+    char buffer[BUFSIZE];
+    char baseURL[BUFSIZE];
+    char firstURL[BUFSIZE];
+    char next[BUFSIZE];
+    int maxURLs;
 
-	if (argc > 2) {
-		strcpy (baseURL, argv[1]);
-		setFirstURL (baseURL, firstURL);
-		maxURLs = atoi (argv[2]);
-		if (maxURLs < 40)
-			maxURLs = 40;
-	} else {
-		fprintf (stderr, "Usage: %s BaseURL MaxURLs\n", argv[0]);
-		exit (1);
-	}
+    if (argc > 2) {
+        strcpy (baseURL, argv[1]);
+        setFirstURL (baseURL, firstURL);
+        maxURLs = atoi (argv[2]);
+        if (maxURLs < 40)
+            maxURLs = 40;
+    } else {
+        fprintf (stderr, "Usage: %s BaseURL MaxURLs\n", argv[0]);
+        exit (1);
+    }
 
-	// You need to modify the code below to implement:
-	//
-	// add firstURL to the ToDo list
-	// initialise Graph to hold up to maxURLs
-	// initialise set of Seen URLs
-	// while (ToDo list not empty and Graph not filled) {
-	//    grab Next URL from ToDo list
-	//    if (not allowed) continue
-	//    foreach line in the opened URL {
-	//       foreach URL on that line {
-	//          if (Graph not filled or both URLs in Graph)
-	//             add an edge from Next to this URL
-	//          if (this URL not Seen already) {
-	//             add it to the Seen set
-	//             add it to the ToDo list
-	//          }
-	//       }
-	//    }
-	//    close the opened URL
-	//    sleep(1)
-	// }
-	Stack stack1 = newStack();
-	Graph graph1 = newGraph(maxURLs);
-	Set set1 = newSet();
-	pushOnto(stack1,firstURL);
-	insertInto(set1,firstURL);
-	while (!emptyStack(stack1) && nVertices(graph1) <= maxURLs){
-		strcpy(firstURL,popFrom(stack1));
-		if (!(handle = url_fopen (firstURL, "r"))) {
-			fprintf (stderr, "Couldn't open %s\n", next);
-			exit (1);
-		}
-		while (!url_feof (handle)) {
-			url_fgets (buffer, sizeof (buffer), handle);
-			// fputs(buffer,stdout);
-			int pos = 0;
-			char result[BUFSIZE];
-			memset (result, 0, BUFSIZE);
-			while ((pos = GetNextURL (buffer, firstURL, result, pos)) > 0) {
-				printf ("Found: '%s'\n", result);
-				if (nVertices(graph1)<= maxURLs || (isElem(set1,firstURL) && isElem(set1,result))){
-					addEdge(graph1,firstURL,result);
-				}
-				if (!isElem(set1,result)){
-					insertInto(set1,result);
-					pushOnto(stack1,result);
-				}
-				memset (result, 0, BUFSIZE);
-			}
-		}
-		url_fclose (handle);
-		sleep (1);
-	}
-	showGraph(graph1,1);
-	dropStack(stack1);
-	dropSet(set1);
-	dropGraph(graph1);
-	return 0;
+    // You need to modify the code below to implement:
+    //
+    // add firstURL to the ToDo list
+    // initialise Graph to hold up to maxURLs
+    // initialise set of Seen URLs
+    // while (ToDo list not empty and Graph not filled) {
+    //    grab Next URL from ToDo list
+    //    if (not allowed) continue
+    //    foreach line in the opened URL {
+    //       foreach URL on that line {
+    //          if (Graph not filled or both URLs in Graph)
+    //             add an edge from Next to this URL
+    //          if (this URL not Seen already) {
+    //             add it to the Seen set
+    //             add it to the ToDo list
+    //          }
+    //       }
+    //    }
+    //    close the opened URL
+    //    sleep(1)
+    // }
+    Queue q = newQueue();
+    Graph graph1 = newGraph(maxURLs);
+    Set set1 = newSet();
+    enterQueue(q,firstURL);
+    insertInto(set1,firstURL);
+    while (!emptyQueue(q) && nVertices(graph1) <= maxURLs){
+        char *url = leaveQueue(q);
+        if (!(handle = url_fopen (url, "r"))) {
+            fprintf (stderr, "Couldn't open %s\n", next);
+            exit (1);
+        }
+        while (!url_feof (handle)) {
+            url_fgets (buffer, sizeof (buffer), handle);
+            // fputs(buffer,stdout);
+            int pos = 0;
+            char result[BUFSIZE];
+            memset (result, 0, BUFSIZE);
+            while ((pos = GetNextURL (buffer, url, result, pos)) > 0) {
+                printf ("Found: '%s'\n", result);
+                if (nVertices(graph1)<= maxURLs || (isElem(set1,url) && isElem(set1,result))){
+                    addEdge(graph1,url,result);
+                }
+                if (!isElem(set1,result)){
+                    insertInto(set1,result);
+                    enterQueue(q,result);
+                }
+                memset (result, 0, BUFSIZE);
+            }
+        }
+        url_fclose (handle);
+        free(url);
+        sleep (1);
+    }
+    showGraph(graph1,1);
+    dropQueue(q);
+    dropSet(set1);
+    dropGraph(graph1);
+    return 0;
 }
 
 // setFirstURL(Base,First)
@@ -104,16 +106,16 @@ int main (int argc, char **argv)
 // - modifies Base to a "normalised" version of itself
 static void setFirstURL (char *base, char *first)
 {
-	char *c;
-	if ((c = strstr (base, "/index.html")) != NULL) {
-		strcpy (first, base);
-		*c = '\0';
-	} else if (base[strlen (base) - 1] == '/') {
-		strcpy (first, base);
-		strcat (first, "index.html");
-		base[strlen (base) - 1] = '\0';
-	} else {
-		strcpy (first, base);
-		strcat (first, "/index.html");
-	}
+    char *c;
+    if ((c = strstr (base, "/index.html")) != NULL) {
+        strcpy (first, base);
+        *c = '\0';
+    } else if (base[strlen (base) - 1] == '/') {
+        strcpy (first, base);
+        strcat (first, "index.html");
+        base[strlen (base) - 1] = '\0';
+    } else {
+        strcpy (first, base);
+        strcat (first, "/index.html");
+    }
 }
